cca464af5a279d2e5a6c030bc9918df46b41660a
Alexander Katin <aleksandr.katin@gmail.com>
2018-02-21 23:56:59 +0300

    Experiment with inputs

--- assets/main.js
+++ assets/main.js
@@ -20,7 +20,6 @@ unitConverter.selectCurrency = function() {
 				var selectedCurrency = $(this).text();
 				var button = $(this).parent().parent().find('button');
 				var buttonCurrency = $(button).text();
-				console.log(selectedCurrency);
 				//take label from key 
 				//and put it in an input
 				$(button).text(selectedCurrency);
@@ -59,15 +58,112 @@ unitConverter.handleErrors = function(mode) {
 		} else {
 			unknownError = 'Wrong input';
 		}
-		$('.result').text('0');
+		$('.result div').text('0');
 		$('.output-container__error p').text('').text(unknownError);
 		$('.output-container__error').removeClass('hidden');
 	}
 }
 
-$(document).ready(function() {
+//Largely based on numericInput.js by Joshua De Leon
+unitConverter.handleInput = function(min, max) {
+	$('#numericinput').keypress(function(event) {
+		var inputCode = event.which;
+		var currentValue = $(this).val();
 
+		//If the value isn't empty
+		if (currentValue.length > 0) {
+			//Get the float value, even if we're not using floats this will be ok
+			var floatValue = parseFloat(currentValue);
+			
+			//If min is specified and the value is less set the value to min
+			if (min !== undefined && floatValue < min) {
+				$(this).blur().val(min);
+			}
+			
+			//If max is specified and the value is less set the value to max
+			if (max !== undefined && floatValue > max) {
+				$(this).blur().val(max);
+			}
+		}
+
+		if (inputCode > 0 && (inputCode < 48 || inputCode > 57)) {
+
+
+			if (inputCode == 46)	// Conditions for a period (decimal point)
+			{
+				
+				//Disallows more than one decimal point.
+				if (currentValue.match(/[.]/)) 
+					return false; 
+			}
+
+			else if (inputCode == 8 || inputCode == 67 || inputCode == 86) 	// Allows backspace , ctrl+c ,ctrl+v (copy & paste)
+				return true; 
+
+			else								// Disallow non-numeric
+				return false; 
+
+
+		}
+
+	});
+
+	$('#numericinput').keyup(function(event) {
+		var currentValue = $(this).val();
+		var result = 0;
+		if (!isNaN(currentValue) && currentValue != '') {
+			result = parseFloat(currentValue) * 1200;
+		} else {
+			result = 0;
+		}
+		fitty('.result div', { maxSize: 32 });
+		$('.result div').text(result);
+	});
+
+	$('.period').click(function() {
+		console.log('simpress');
+		var e = jQuery.Event('keydown');
+		e.which = 190;
+		e.keycode = 190;
+		$('#numericinput').focus().trigger(e);
+		// simulateKeyPress('.');
+	});
+
+	function simulateKeyPress(character) {
+	  // jQuery.event.trigger({ type : 'keypress', which : character.charCodeAt(0) });
+	}
+
+}
+
+// Private function for selecting cursor position. Makes IE play nice.
+// http://stackoverflow.com/questions/263743/how-to-get-caret-position-in-textarea
+unitConverter.getCaret = function(element) {
+	if (element.selectionStart) {
+		return element.selectionStart; 
+	}
+
+	else if (document.selection) //IE specific
+	{ 
+		element.focus(); 
+
+		var r = document.selection.createRange(); 
+		if (r == null) 
+			return 0; 
+
+		var re = element.createTextRange(), 
+		rc = re.duplicate(); 
+		re.moveToBookmark(r.getBookmark()); 
+		rc.setEndPoint('EndToStart', re); 
+		return rc.text.length; 
+	}  
+
+	return 0;
+
+}
+
+$(document).ready(function() {
+	
+	unitConverter.handleInput(0, 999999999.99);
 	unitConverter.switchKeypads();
 	unitConverter.selectCurrency();
-
 });
